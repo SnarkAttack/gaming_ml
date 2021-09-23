1936b4594997aab36056cb1ea03ef0e16d54326e
Patrick McQuay <patrick.mcquay@gmail.com>
2021-09-22 22:42:34 -0400

    Move next move logic from interface to player

--- src/predefined_games/connect_four/player.py
+++ src/predefined_games/connect_four/player.py
@@ -1,4 +1,6 @@
 from src.game.player import Player
+from src.game.utilities import HUMAN_CODE, COMPUTER_CODE
+from .action import PlaceDiscAction
 
 class ConnectFourPlayer(Player):
 
@@ -14,3 +16,20 @@ class ConnectFourPlayer(Player):
     @property
     def symbol(self):
         return self._symbol
+
+    def get_next_move(self):
+        if self.code == HUMAN_CODE:
+            choice = None
+            available_columns = self._game.get_valid_columns()
+            print(f"Player {self.symbol}, what column would you like to play in?\n")
+            while choice not in available_columns:
+                try:
+                    choice = int(input(f"Select from the following list: {available_columns}\t"))
+                except Exception:
+                    print("Invalid input")
+            game_state = self._game.perform_action(PlaceDiscAction(self, choice))
+            return game_state
+
+    def _get_valid_moves(self):
+        available_columns = self._game.get_valid_columns()
+        return [PlaceDiscAction(self, col) for col in available_columns]
