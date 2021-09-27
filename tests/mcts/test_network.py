12ee2437668340165fd6b7d5502c070141cd8e78
Patrick McQuay <patrick.mcquay@gmail.com>
2021-09-26 22:46:13 -0400

    Finished base network code

--- tests/mcts/test_network.py
+++ tests/mcts/test_network.py
@@ -1,5 +1,5 @@
 import numpy as np
-from src.mcts.network import ConvolutionalBlock, RedisualBlock
+from src.mcts.network import ConvolutionalBlock, Network
 
 def test_convolutional_block():
     conv_block = ConvolutionalBlock(
@@ -26,5 +26,39 @@ def test_convolutional_block():
 
     x = conv_block(a)
 
-    assert x.shape == (1, 2, 2, 128)
+    assert x.shape == (1, 3, 3, 128)
 
+def test_network():
+
+    network = Network(
+        filters=256,
+        kernel_size=(2, 2),
+        strides=(1,1),
+        policy_options = 10
+    )
+
+    
+    board = np.stack([
+        [
+            [1, 2, 3],
+            [3, 4, 5],
+            [4, 5, 6]
+        ],
+        [
+            [5, 6, 7],
+            [7, 8, 9],
+            [8, 9, 0]
+        ]
+    ], axis=2).astype(np.float)
+
+    assert board.shape == (3, 3, 2)
+
+    a = np.array([board])
+
+    val, policy = network(a)
+
+    val = val.numpy()[0]
+    policy = policy.numpy()[0]
+
+    assert val >= -1 and val <= 1
+    assert round(sum(policy), 5) == 1
